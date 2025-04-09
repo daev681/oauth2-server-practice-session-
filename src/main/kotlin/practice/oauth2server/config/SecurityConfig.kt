57ee0a5ac36cfe70745f4c18e8a00f0dbb2788d3
Park DaeHeon <wow1186@naver.com>
2025-04-09 11:28:48 +0900

    modify : userDetaile 수정

--- src/main/kotlin/practice/oauth2server/config/SecurityConfig.kt
+++ src/main/kotlin/practice/oauth2server/config/SecurityConfig.kt
@@ -1,4 +1,5 @@
 package practice.oauth2server.config
+
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
@@ -8,7 +9,6 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
@@ -18,9 +18,12 @@ class SecurityConfig {
 
     @Bean
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
-        return http.formLogin(Customizer.withDefaults()).build()
+        http
+                .authorizeHttpRequests { it.anyRequest().authenticated() }
+                .formLogin(Customizer.withDefaults())
+        return http.build()
     }
+
 
     @Bean
     fun userDetailsService(): UserDetailsService {
@@ -29,7 +32,6 @@ class SecurityConfig {
                 .roles("USER")
                 .build()
         println("✅ InMemoryUser 등록됨: ${user.username}")
-
         return InMemoryUserDetailsManager(user)
     }
 
